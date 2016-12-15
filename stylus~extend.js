c54c9aa32cabe34c2cbeafc146cf54fbfbb1a4be
e2tha-e <ee@electric-eloquence.io>
2016-12-15 08:27:44 -0500

    fp-stylus updated to work with latest fepper build

--- stylus~extend.js
+++ stylus~extend.js
@@ -1,29 +1,35 @@
 'use strict';
 
-var conf = global.conf;
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
-var runSequence = require('run-sequence');
-var utilsGulp = require('../../../gulp/utils');
+const conf = global.conf;
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+const runSequence = require('run-sequence');
+
+const appDir = global.appDir;
+const utilsCore = require(`${appDir}/core/lib/utils`);
+const utilsTask = require(`${appDir}/tasker/utils`);
+
+const cssBldDir = utilsCore.pathResolve(conf.ui.paths.source.cssBld);
+const cssSrcDir = utilsCore.pathResolve(conf.ui.paths.source.cssSrc);
 
 gulp.task('stylus', function () {
-  return gulp.src('./' + conf.src + '/css-processors/stylus/*.styl')
+  return gulp.src(cssSrcDir + '/stylus/*.styl')
     .pipe(plugins.stylus({
       linenos: true
     }))
-    .on('error', utilsGulp.handleError)
-    .pipe(gulp.dest('./' + conf.src + '/_styles'));
+    .on('error', utilsTask.handleError)
+    .pipe(gulp.dest(cssBldDir));
 });
 
 // This runs the CSS processor without outputting line comments.
 // You probably want this to process CSS destined for production.
 gulp.task('stylus:no-comments', function () {
-  return gulp.src('./' + conf.src + '/css-processors/stylus/*.styl')
+  return gulp.src(cssSrcDir + '/stylus/*.styl')
     .pipe(plugins.stylus({
       linenos: false
     }))
-    .on('error', utilsGulp.handleError)
-    .pipe(gulp.dest('./' + conf.src + '/_styles'));
+    .on('error', utilsTask.handleError)
+    .pipe(gulp.dest(cssBldDir));
 });
 
 gulp.task('stylus:frontend-copy', function (cb) {
@@ -35,5 +41,5 @@ gulp.task('stylus:frontend-copy', function (cb) {
 });
 
 gulp.task('stylus:watch', function () {
-  gulp.watch('./' + conf.src + '/css-processors/stylus/**/*.styl', ['stylus']);
+  gulp.watch('stylus/**', {cwd: cssSrcDir}, ['stylus']);
 });
