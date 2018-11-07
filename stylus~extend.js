8af8749fb0c67ce488b7988a56246b70ad17193c
e2tha-e <ee@electric-eloquence.io>
2018-11-07 11:12:55 -0500

    split out stylus:diff-then-no-comment task, closures instead of creating functions in loops

--- stylus~extend.js
+++ stylus~extend.js
@@ -14,28 +14,7 @@ const conf = global.conf;
 const cssBldDir = conf.ui.paths.source.cssBld;
 const cssSrcDir = conf.ui.paths.source.cssSrc;
 
-function handleError(err) {
-  utils.error(err.toString());
-  this.emit('end');
-}
-
-gulp.task('stylus', function () {
-  return gulp.src(cssSrcDir + '/stylus/*.styl')
-    .pipe(gulpStylus({
-      linenos: true
-    }))
-    .on('error', handleError)
-    .pipe(gulp.dest(cssBldDir));
-});
-
-// This first checks if the old bld CSS has line comments. If so, it runs the 'stylus' task and returns.
-// If there are no line comments, it processes Stylus without line comments, to compare the new CSS with the old CSS.
-// If there's a difference, it then processes Stylus again with line comments and writes that CSS to the bld directory.
-// Otherwise, it leaves the old bld CSS alone.
-// The intended result is for users who use Fepper defaults never to notice Stylus being processed if they never edit
-// Stylus files, and for users who do edit Stylus files to have Stylus process as expected.
-// Power-users should replace this with the 'stylus:once' or 'stylus:no-comment' task for better performance.
-gulp.task('stylus:diff-then-comment', function (cb) {
+function diffThenRender(commentBool, cb) {
   const cssFilesBld = fs.readdirSync(cssBldDir);
   let hasComments = false;
   let i = cssFilesBld.length;
@@ -67,113 +46,140 @@ gulp.task('stylus:diff-then-comment', function (cb) {
   }
 
   const stylDir = `${cssSrcDir}/stylus`;
-
-  process.chdir(stylDir);
-
   const stylFiles = fs.readdirSync(stylDir);
+  i = stylFiles.length;
 
-  // Promise so we chdir back to global.rootDir at completion of async functions.
-  new Promise((resolve) => {
-    let i = stylFiles.length;
+  while (i--) {
+    const stylFile = `${stylDir}/${stylFiles[i]}`;
+    const stylFileObj = path.parse(stylFile);
 
-    while (i--) {
-      const stylFile = stylFiles[i];
-      const stylFileObj = path.parse(stylFile);
-
-      if (stylFileObj.ext !== '.styl') {
-        if (i === 0) {
-          resolve();
-        }
-
-        continue;
-      }
-
-      const stat = fs.statSync(stylFile);
-
-      if (!stat.isFile() && !stat.isSymbolicLink()) {
-        if (i === 0) {
-          resolve();
-        }
-
-        continue;
-      }
-
-      const stylFileStr = fs.readFileSync(stylFile, conf.enc);
-
-      stylus(stylFileStr)
-        .set('filename', stylFile)
-        .set('linenos', false)
-        .render((err, cssNew) => {
-          if (err) {
-            utils.error(err);
-          }
-          else {
-            // Declare tmp file for comparison.
-            const cssFileTmp = `${cssSrcDir}/${stylFileObj.name}.css`;
-            let cssFileTmpStr = '';
-
-            if (fs.existsSync(cssFileTmp)) {
-              cssFileTmpStr = fs.readFileSync(cssFileTmp, conf.enc);
-            }
-
-            // Compare newly rendered css with the contents of the tmp file.
-            // Exit if there has been no change. This is the case for users who only edit bld css and do not modify
-            // Stylus files.
-            if (cssFileTmpStr === cssNew) {
-              if (i === 0) {
-                resolve();
-              }
-
-              return;
-            }
-
-            // Output tmp file for future comparison.
-            fs.outputFileSync(cssFileTmp, cssNew);
-
-            // Now, compare against bld css.
-            const cssFileBld = `${cssBldDir}/${stylFileObj.name}.css`;
-            const stat = fs.statSync(cssFileBld);
-
-            if (stat.isFile()) {
-              const cssOld = fs.readFileSync(cssFileBld, conf.enc);
-
-              // The first time 'stylus:diff-then-comment' is run, cssNew should equal cssOld.
-              // cssFileTmp will have been written at this point and will be used for future comparisons.
-              // If users only edit bld css and do not modify Stylus files, they should never get to this point.
-              if (cssNew !== cssOld) {
-                stylus(stylFileStr)
-                  .set('filename', stylFile)
-                  .set('linenos', true)
-                  .render((err1, cssNewCommented) => {
-                    if (err1) {
-                      utils.error(err1);
-                    }
-                    else {
-                      fs.outputFileSync(cssFileBld, cssNewCommented);
-                    }
-
-                    if (i === stylFiles.length - 1) {
-                      resolve();
-                    }
-                  });
-              }
-            }
-          }
-
-          if (i === 0) {
-            resolve();
-          }
-        });
-
+    if (stylFileObj.ext !== '.styl') {
       if (i === 0) {
-        resolve();
+        cb();
       }
+
+      continue;
     }
-  })
-  .then(() => {
-    process.chdir(global.rootDir);
-    cb();
-  });
+
+    const stat = fs.statSync(stylFile);
+
+    if (!stat.isFile()) {
+      if (i === 0) {
+        cb();
+      }
+
+      continue;
+    }
+
+    const stylFileStr = fs.readFileSync(stylFile, conf.enc);
+
+    stylus(stylFileStr)
+      .set('filename', stylFile)
+      .set('linenos', false)
+      .render(
+        ((iteration) => {
+          return (err, cssNew) => {
+            if (err) {
+              utils.error(err);
+            }
+            else {
+              // Declare tmp file for comparison.
+              const cssFileTmp = `${cssSrcDir}/${stylFileObj.name}.css`;
+              let cssFileTmpStr = '';
+
+              if (fs.existsSync(cssFileTmp)) {
+                cssFileTmpStr = fs.readFileSync(cssFileTmp, conf.enc);
+              }
+
+              // Compare newly rendered css with the contents of the tmp file.
+              // Exit if there has been no change. This is the case for users who only edit bld css and do not modify
+              // Stylus files.
+              if (cssFileTmpStr === cssNew) {
+                if (iteration === 0) {
+                  cb();
+                }
+
+                return;
+              }
+
+              // Output tmp file for future comparison.
+              fs.outputFileSync(cssFileTmp, cssNew);
+
+              // Now, compare against bld css.
+              const cssFileBld = `${cssBldDir}/${stylFileObj.name}.css`;
+              const stat = fs.statSync(cssFileBld);
+
+              if (stat.isFile()) {
+                const cssOld = fs.readFileSync(cssFileBld, conf.enc);
+
+                // The first time 'stylus:diff-then-comment' is run, cssNew should equal cssOld.
+                // cssFileTmp will have been written at this point and will be used for future comparisons.
+                // If users only edit bld css and do not modify Stylus files, they should never get to this point.
+                if (cssNew !== cssOld) {
+                  stylus(stylFileStr)
+                    .set('filename', stylFile)
+                    .set('linenos', commentBool)
+                    .render(
+                      ((iteration1) => {
+                        return (err1, cssNewCommented) => {
+                          if (err1) {
+                            utils.error(err1);
+                          }
+                          else {
+                            fs.outputFileSync(cssFileBld, cssNewCommented);
+                          }
+
+                          if (iteration1 === 0) {
+                            cb();
+                          }
+                        };
+                      })(iteration)
+                    );
+                }
+              }
+            }
+
+            if (iteration === 0) {
+              cb();
+            }
+          };
+        })(i)
+      );
+
+    if (i === 0) {
+      cb();
+    }
+  }
+}
+
+function handleError(err) {
+  utils.error(err.toString());
+  this.emit('end');
+}
+
+gulp.task('stylus', function () {
+  return gulp.src(cssSrcDir + '/stylus/*.styl')
+    .pipe(gulpStylus({
+      linenos: true
+    }))
+    .on('error', handleError)
+    .pipe(gulp.dest(cssBldDir));
+});
+
+// This first checks if the old bld CSS has line comments. If so, it runs the 'stylus' task and returns.
+// If there are no line comments, it processes Stylus without line comments, to compare the new CSS with the old CSS.
+// If there's a difference, it then processes Stylus again with line comments and writes that CSS to the bld directory.
+// Otherwise, it leaves the old bld CSS alone.
+// The intended result is for users who use Fepper defaults never to notice Stylus being processed if they never edit
+// Stylus files, and for users who do edit Stylus files to have Stylus process as expected.
+// Power-users should replace this with the 'stylus:once' or 'stylus:no-comment' task for better performance.
+gulp.task('stylus:diff-then-comment', function (cb) {
+  diffThenRender(true, cb);
+});
+
+// Same as 'stylus:diff-then-comment' but with no line comments in rendered CSS.
+gulp.task('stylus:diff-then-no-comment', function (cb) {
+  diffThenRender(false, cb);
 });
 
 // This runs the CSS processor without outputting line comments.
@@ -189,7 +195,7 @@ gulp.task('stylus:no-comment', function () {
 
 gulp.task('stylus:frontend-copy', function (cb) {
   runSequence(
-    'stylus:no-comment',
+    'stylus:diff-then-no-comment',
     'ui:copy-styles',
     cb
   );
